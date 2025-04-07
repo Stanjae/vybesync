df518d2c2f9d3a00a62f579f5a6484a3ba722f7e
stanjae <stanykhay29@gmail.com>
2025-04-07 06:28:51 -0400

    maidenpool

--- src/components/cui/CVideoCard.tsx
+++ src/components/cui/CVideoCard.tsx
@@ -49,7 +49,7 @@ useEffect(()=>{
   const [show, setShow]= useState(false)
   return (
     <div id={`video-${id}`} className=' bg-foreground text-background h-screen containt overflow-hidden relative justify-center pt-5 gap-4 block sm:flex'>
-      <section ref={ref} id='foryou-id' className=' px-5 md:px-0 relative w-full sm:w-[419px]'>
+      <section ref={ref} id='foryou-id' className='z-10 px-5 md:px-0 relative w-full sm:w-[419px]'>
         <CosVideoPlayer id={item?._id} key={item?._id} videoRef={vidref} className=' border-none outline-none rounded-2xl'
         width="419"
         height="744"
@@ -66,10 +66,10 @@ useEffect(()=>{
         }}
         onEnded={()=> updateViewCount(item?._id)}
         />
-        <div className=' [text-shadow:_0_4px_8px_#ea284e] shadow-2xl  text-center text-pretty text-2xl w-full bg-foreground/50 absolute left-1/2 top-1/12 -translate-x-1/2'>
+        <div className=' [text-shadow:_0_4px_8px_#ea284e] z-30 shadow-2xl  text-center text-pretty text-2xl w-full bg-foreground/50 absolute left-1/2 top-1/12 -translate-x-1/2'>
         {item?.caption}
         </div>
-        <div className=' w-11/12 absolute bottom-11 space-y-1 left-3'>
+        <div className=' w-11/12 z-30 absolute bottom-11 space-y-1 left-3'>
           <a href={`/@${item?.author?.name?.toLowerCase()}`} className=' leading-[21px] text-background font-semibold text-base flex gap-1 items-center'>{item?.author?.fullname} <LucideBadgeCheck className=' text-[#20d5ec]'/></a>
           <div onClick={()=> setShow(prev => !prev)}  className={` ${show ? "text-pretty transition-all duration-500":" transition-all duration-500 overflow-hidden text-ellipsis whitespace-nowrap"} bg-foreground/10 transition-all duration-500  text-sm text-background/85`}>
             {item?.description}
@@ -81,7 +81,7 @@ useEffect(()=>{
       </section>
         
 
-         <div className=' absolute z-50 top-1/2 -translate-y-1/2 md:translate-y-0 right-5 md:right-0 md:relative md:-top-5 flex flex-col gap-4 items-center justify-end'>
+         <div className=' absolute z-40 top-1/2 -translate-y-1/2 md:translate-y-0 right-5 md:right-0 md:relative md:-top-5 flex flex-col gap-4 items-center justify-end'>
           {/* author follow */}
             <FollowAuthorMini userId={session?.user?.id as string} item={item?.author} />
 
